1ea0d7a50ab1cf69dc2b944ed4c763aaceeb3309
Steffen Schwigon <ss5@renormalist.net>
2015-08-07 01:58:35 +0200

    POD documentation

--- lib/BenchmarkAnything/Schema.pm
+++ lib/BenchmarkAnything/Schema.pm
@@ -3,6 +3,30 @@ use warnings;
 package BenchmarkAnything::Schema;
 # ABSTRACT: Tooling to handle the "BenchmarkAnything" schema
 
+=head1 SYNOPSIS
+
+ require BenchmarkAnything::Schema;
+
+ if (my $result = BenchmarkAnything::Schema::valid_json_schema($input_data__or__raw_json_text))
+ {
+     print "Data structure is valid BenchmarkAnything data.\n";
+ } else {
+     print STDERR "JSON schema errors:\n";
+     print STDERR " - $_\n" foreach $result->errors;
+ }
+
+=cut
+
+=head2 valid_json_schema($data_or_json)
+
+Validate if $data_or_json conforms to the BenchmarkAnything schema.
+
+Returns a L<JSON::Schema::Result|JSON::Schema::Result> object which is
+overloaded to behave and stringify sensibly, and also provides error
+details.
+
+=cut
+
 sub valid_json_schema {
     my ($data_or_json) = @_;
 
